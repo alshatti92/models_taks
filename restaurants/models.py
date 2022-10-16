772f6c07ea62de69cbbf60a0b734ddcbb9d35d14
alshatti92 <a.alshatti@hotmail.co.uk>
2022-10-16 16:45:32 +0300

    fixed max_legnth argument

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,9 +1,9 @@
-from unittest.util import _MAX_LENGTH
+from unittest.util import max_legnth
 from django.db import models
 
 # Create your models here.
 class Restaurant(models.model):
-    name = models.CharField(_MAX_LENGTH=30)
+    name = models.CharField(max_legnth=30)
     description = models.TextField(default="")
     openning_time = models.TimeField()
     closing_time = models.TimeField()
